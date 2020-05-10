22ba4446f5e961cbbb93d33b71d0a20c01203d00
sham-sheer <shamsheer619@gmail.com>
2020-05-10 14:58:06 +0800

    first commit: Firebase Integration

---
+++ app/src/main/java/com/example/etows/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.etows;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button nTower, nCustomer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nTower = (Button) findViewById(R.id.tower);
+        nCustomer = (Button) findViewById(R.id.customer);
+
+        nTower.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TowerLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        nCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CustomerLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/etows/TowerLoginActivity.java
@@ -0,0 +1,108 @@
+package com.example.etows;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class TowerLoginActivity extends AppCompatActivity {
+    private EditText nEmail, nPassword;
+    private Button nLogin, nRegistration;
+
+    private FirebaseAuth nAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tower_login);
+
+        nAuth = FirebaseAuth.getInstance();
+
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if(user != null) {
+                    Intent intent = new Intent(TowerLoginActivity.this, MapActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+            }
+        };
+
+        nAuth.addAuthStateListener(firebaseAuthListener);
+
+        nEmail = (EditText) findViewById(R.id.email);
+        nPassword = (EditText) findViewById(R.id.password);
+
+        nLogin = (Button) findViewById(R.id.login);
+        nRegistration = (Button) findViewById(R.id.registration);
+
+        nRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = nEmail.getText().toString();
+                final String password = nEmail.getText().toString();
+
+                nAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(TowerLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()) {
+                            Toast.makeText(TowerLoginActivity.this, "Failed to sign up: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            String user_id = nAuth.getCurrentUser().getUid();
+                            DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Towers").child(user_id);
+                            current_user_db.setValue(true);
+                        }
+                    }
+                });
+            }
+        });
+
+        nLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = nEmail.getText().toString();
+                final String password = nEmail.getText().toString();
+
+                nAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(TowerLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()) {
+                            Toast.makeText(TowerLoginActivity.this, "Failed to sign in", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        nAuth.removeAuthStateListener(firebaseAuthListener);
+    }
+}
