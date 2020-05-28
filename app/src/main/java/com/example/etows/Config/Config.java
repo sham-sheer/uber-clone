77319df4fd636e11e44677ea932d34c5721d00c9
sham-sheer <shamsheer619@gmail.com>
2020-05-28 21:50:13 +0800

    Paypal intgration support

---
+++ app/src/main/java/com/example/etows/Config/Config.java
@@ -0,0 +1,5 @@
+package com.example.etows.Config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "AVHrgeW8s5vxZi-kjn_IL90fEFcpcypozCfnl-vNuq3u5iFQ_BcVPBPks4IZA0az4ca2fa2-6HdTIMgV";
+}
--- app/src/main/java/com/example/etows/HistorySingleActivity.java
+++ app/src/main/java/com/example/etows/HistorySingleActivity.java
@@ -1,16 +1,21 @@
 package com.example.etows;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.akexorcist.googledirection.DirectionCallback;
 import com.akexorcist.googledirection.GoogleDirection;
@@ -32,7 +37,16 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.example.etows.Config.Config;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 
+import org.json.JSONException;
+
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -60,6 +74,13 @@ public class HistorySingleActivity extends AppCompatActivity implements OnMapRea
 
     private RatingBar mRatingBar;
 
+    private Button mPay;
+
+    private static final int PAYPAL_REQUEST_CODE = 7777;
+
+    private static PayPalConfiguration config = new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+            .clientId(Config.PAYPAL_CLIENT_ID);
 
     private GoogleMap mMap;
     private SupportMapFragment mMapFragment;
@@ -82,11 +103,14 @@ public class HistorySingleActivity extends AppCompatActivity implements OnMapRea
 
         mRatingBar = (RatingBar) findViewById(R.id.ratingBar);
 
+        mPay = (Button) findViewById(R.id.pay);
+
         historyRideInfoDb = FirebaseDatabase.getInstance().getReference().child("history").child(rideId);
         getRideInformation();
 
         mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mMapFragment.getMapAsync(this);
+
     }
 
     private void getRideInformation() {
@@ -148,6 +172,7 @@ public class HistorySingleActivity extends AppCompatActivity implements OnMapRea
 
     private void displayCustomerRelatedObjects() {
         mRatingBar.setVisibility(View.VISIBLE);
+        mPay.setVisibility(View.VISIBLE);
         mRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
@@ -157,6 +182,44 @@ public class HistorySingleActivity extends AppCompatActivity implements OnMapRea
             }
         });
 
+        mPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                processPayment();
+            }
+        });
+
+    }
+
+    private void processPayment() {
+        PayPalPayment payPalPayment = new PayPalPayment(new BigDecimal(String.valueOf(ridePrice)),"USD",
+                "Purchase Goods",PayPalPayment.PAYMENT_INTENT_SALE);
+        Intent intent = new Intent(this, PaymentActivity.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,config);
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT,payPalPayment);
+        startActivityForResult(intent,PAYPAL_REQUEST_CODE);
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PAYPAL_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if (confirmation != null) {
+                    try {
+                        String paymentDetails = confirmation.toJSONObject().toString(4);
+                        startActivity(new Intent(this, PaymentDetails.class)
+                                .putExtra("Payment Details", paymentDetails)
+                                .putExtra("Amount", ridePrice));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED)
+                Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show();
+        } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID)
+            Toast.makeText(this, "Invalid", Toast.LENGTH_SHORT).show();
     }
 
     private void getUserInformation(String otherDriverOrCustomer, String otherUserId) {
